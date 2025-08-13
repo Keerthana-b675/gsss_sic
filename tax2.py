c84ad04a12944feca616c7ee5d4e00a8a38225d9
Keerthana-b675 <keerthanab675@gmail.com>
2025-08-13 11:55:38 +0530

    Delete tax2.py

--- tax2.py
+++
@@ -1,5 +0,0 @@
-import tax1
-Standard_Deduction=50_000.00
-print("Standard_Deduction is:", Standard_Deduction)
-Taxable_income= tax1.Gross_Annual_Salary - Standard_Deduction
-print("Taxable Income is:", Taxable_income)
